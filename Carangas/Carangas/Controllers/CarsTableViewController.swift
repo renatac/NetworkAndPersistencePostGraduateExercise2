2eb45af715de37fd939c4b506a5e3e4420f3e4fd
Renata <renatacristovams@gmail.com>
2021-05-16 02:06:31 -0300

    Putting Side Menu

--- Carangas/Carangas/Controllers/CarsTableViewController.swift
+++ Carangas/Carangas/Controllers/CarsTableViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import SideMenu
 
 class CarsTableViewController: UITableViewController {
-
+    
     var cars: [Car] = []
     
     
@@ -20,8 +21,23 @@ class CarsTableViewController: UITableViewController {
         return label
     }()
     
+    fileprivate func recognizeMenuGestures() {
+        // Define the menus
+        SideMenuManager.default.leftMenuNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as? SideMenuNavigationController
+        
+        // Enable gestures. The left and/or right menus must be set up above for these to work.
+        // Note that these continue to work on the Navigation Controller independent of the View Controller it displays!
+        
+        SideMenuManager.default.addPanGestureToPresent(toView: self.navigationController!.navigationBar)
+        // Updated
+        SideMenuManager.default.addScreenEdgePanGesturesToPresent(toView: self.navigationController!.view, forMenu: SideMenuManager.PresentDirection.left)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        recognizeMenuGestures()
+        
         
         label.text = NSLocalizedString("Carregando dados...", comment: "")
         
@@ -30,6 +46,8 @@ class CarsTableViewController: UITableViewController {
         refreshControl?.addTarget(self, action: #selector(loadData), for: .valueChanged)
         tableView.refreshControl = refreshControl
     }
+    
+    
     
     @objc func loadData() {
         
@@ -65,19 +83,19 @@ class CarsTableViewController: UITableViewController {
             var response: String = ""
             
             switch error {
-                case .invalidJSON:
-                    response = "invalidJSON"
-                case .noData:
-                    response = "noData"
-                case .noResponse:
-                    response = "noResponse"
-                case .url:
-                    response = "JSON inválido"
-                case .taskError(let error):
-                    response = "\(error.localizedDescription)"
-                case .responseStatusCode(let code):
-                    if code != 200 {
-                        response = "Algum problema com o servidor. :( \nError:\(code)"
+            case .invalidJSON:
+                response = "invalidJSON"
+            case .noData:
+                response = "noData"
+            case .noResponse:
+                response = "noResponse"
+            case .url:
+                response = "JSON inválido"
+            case .taskError(let error):
+                response = "\(error.localizedDescription)"
+            case .responseStatusCode(let code):
+                if code != 200 {
+                    response = "Algum problema com o servidor. :( \nError:\(code)"
                 }
             }
             
@@ -91,24 +109,24 @@ class CarsTableViewController: UITableViewController {
         
         loadData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         if cars.count == 0 {
             
             // mostrar mensagem padrao
-//            self.label.text = "Sem dados"
+            //            self.label.text = "Sem dados"
             self.tableView.backgroundView = self.label
         } else {
             self.label.text = ""
@@ -118,7 +136,7 @@ class CarsTableViewController: UITableViewController {
         
         return cars.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
@@ -130,19 +148,19 @@ class CarsTableViewController: UITableViewController {
         return cell
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-                
+        
         if editingStyle == .delete {
             
             let car = cars[indexPath.row]
@@ -164,25 +182,25 @@ class CarsTableViewController: UITableViewController {
         }
     }
     
-
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
@@ -196,5 +214,5 @@ class CarsTableViewController: UITableViewController {
         }
     }
     
-
+    
 }
